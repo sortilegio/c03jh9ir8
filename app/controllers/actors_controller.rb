3095dd00c7e4fb859fb82b6074658d552697142e
sortilegio <johannacastro8407@gmail.com>
2017-09-08 11:20:55 -0500

    Crear actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,4 +7,21 @@ class ActorsController < ApplicationController
   def show
     @actor = Actor.find(params[:id])
   end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+     if @actor.save
+       redirect_to :actors
+     else
+       render :new
+     end
+  end
+
+  def actor_params
+        params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
+    end
 end
